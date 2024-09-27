aa1582fae26c2fa61ea8c37fd484e35e25fe0ab0
A-M-D-R-3-W <holygsauce@gmail.com>
2024-09-26 18:11:18 -0700

    Clean up source

--- Assignment-2/shapes.js
+++ Assignment-2/shapes.js
@@ -1,3 +1,8 @@
+//
+//  Assingment 2: 3D Shapes
+//  Andrew Garberolio
+//
+
 
 let gl = undefined;
 
@@ -6,7 +11,6 @@ function init() { // init can be defined as a lambda function (see examples)
     gl = canvas.getContext("webgl2");
     if (!gl) { alert("Your Web browser doesn't support WebGL 2\nPlease contact Dave"); }
 
-    // Add initialization code here
 
     gl.clearColor(0.2, 0.2, 0.2, 1.0); // Background color
     gl.enable(gl.DEPTH_TEST); // Enable depth test
@@ -26,66 +30,71 @@ function init() { // init can be defined as a lambda function (see examples)
 
 function render(cone, tetra, cyl, axes, ms, angle) { // if canvas is blank, render is broken
 
-    gl.clear(gl.COLOR_BUFFER_BIT | gl.DEPTH_BUFFER_BIT); // Clear the color as well as the depth buffer (part of the depth test)
+    // Clear the color as well as the depth buffer (part of the depth test)
+    gl.clear(gl.COLOR_BUFFER_BIT | gl.DEPTH_BUFFER_BIT);
 
     // Update the rotation angle (for all shapes that are animated)
     angle += 1.0;
     angle %= 360.0;
 
 
-    // ----------------- Drawing the Axes ----------------- //
+    // TRS (Translate -> Rotate -> Scale) Boilerplate
+
+    // ms.push();
+    // translate(z, y, x);
+    // rotate(angle, [x, y, z]);
+    // scale(s);
+    // shape.MV = ms.current();
+    // shape.draw();
+    // ms.pop();
+
+
+    // ----------------- Drawing the Axes (stationary) ----------------- //
 
     ms.push();
-    ms.translate(0.3, -0.6, 0); // translate(z, y, x)
-    ms.rotate(40, [0, 1, 0]);    // rotate(angle, [x, y, z])
-    ms.rotate(25, [1, 0, 0]);
-    ms.scale(0.5);              // scale
+    ms.translate(0.3, -0.6, 0);     // move to bottom right
+    ms.rotate(40, [0, 1, 0]);       // rotate 40 degrees around y-axis
+    ms.rotate(25, [1, 0, 0]);       // rotate 25 degrees around x-axis
+    ms.scale(0.5);                  // scale 50%
     axes.MV = ms.current();
     axes.draw();
     ms.pop();
 
-
-    // ---------------------------------------------------- //
+    // --------------------------------------------------------------- //
 
 
     // ----------------- Drawing the Cylinder ----------------- //
 
     ms.push();
-    ms.translate(-0.5, 0.5, 0)           // translate(z, y, x)
-    ms.rotate(angle, [1, 1, 0]);    // rotate(angle, [x, y, z])
-    ms.scale(0.3);                  // scale(s)
+    ms.translate(-0.5, 0.5, 0)      // move to top left
+    ms.rotate(angle, [1, 1, 0]);    // rotate {angle} degrees around [1, 1, 0] (diagonal, animated)
+    ms.scale(0.3);                  // scale 30%
     cyl.MV = ms.current();
     cyl.draw();
     ms.pop();
 
-
-    // ---------------------------------------------------- //
-
-
+    // ------------------------------------------------------- //
 
 
     // ----------------- Drawing the Tetrahedron (stationary) ----------------- //
 
     ms.push();
-    ms.translate(-0.5, 0, 0)        // translate(z, y, x)
-    //ms.rotate(angle, [1, 1, 0]);    // rotate(angle, [x, y, z])
-    ms.scale(0.4);                  // scale(s)
+    ms.translate(-0.5, 0, 0)        // move to the bottom left
+    //ms.rotate(angle, [1, 1, 0]);  // how to rotate the tetrahedron
+    ms.scale(0.4);                  // scale 40%
     tetra.MV = ms.current();
     tetra.draw();
     ms.pop();
-
 
     // ---------------------------------------------------- //
 
 
     // ----------------- Drawing the Cone ----------------- //
-    // Transforms should follow this order (TRS): Translate -> Rotate -> Scale
 
-    // Draw the cone
     ms.push();
-    ms.translate(0.5, 0, 0)         // translate(z, y, x)
-    ms.rotate(angle, [1, 1, 0]);    // rotate(angle, [x, y, z])
-    ms.scale(0.4);                  // scale(s)
+    ms.translate(0.5, 0, 0)         // move to the top right
+    ms.rotate(angle, [1, 1, 0]);    // rotate {angle} degrees around [1, 1, 0] (diagonal, animated)
+    ms.scale(0.4);                  // scale 40%
     cone.MV = ms.current();
     cone.draw();
     ms.pop();
@@ -94,8 +103,9 @@ function render(cone, tetra, cyl, axes, ms, angle) { // if canvas is blank, rend
 
 
 
-
-    requestAnimationFrame(() => render(cone, tetra, cyl, axes, ms, angle)); // Call render() again (anonymously)
+    // Call render() again (anonymously)
+    // This is the only way I could get it to work!
+    requestAnimationFrame(() => render(cone, tetra, cyl, axes, ms, angle));
 
 }
 
