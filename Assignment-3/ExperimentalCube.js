bec5c99d54f2c47295d030c1bfeecb32d679e590
A-M-D-R-3-W <holygsauce@gmail.com>
2024-10-21 17:19:51 -0700

    Experimental cube color functioning, but too bright

--- Assignment-3/ExperimentalCube.js
+++ Assignment-3/ExperimentalCube.js
@@ -25,6 +25,8 @@ class ExperimentalCube {
 
             uniform mat4 P;  // Projection transformation
             uniform mat4 MV; // Model-view transformation
+
+            out vec4 vColor;
         
             void main() {
 
@@ -34,18 +36,8 @@ class ExperimentalCube {
                 // 3 components per vertex (x, y, z)
                 // Counter-clockwise winding order
 
-                const vec3 vertices[] = vec3[8](
-                    vec3(0.5, 0.5, 0.5),      // position 0
-                    vec3(-0.5, 0.5, 0.5),     // position 1
-                    vec3(-0.5, -0.5, 0.5),    // position 2
-                    vec3(-0.5, -0.5, -0.5),   // position 3
-                    vec3(0.5, -0.5, -0.5),   // position 4
-                    vec3(0.5, 0.5, -0.5),   // position 5
-                    vec3(0.5, -0.5, 0.5),   // position 6
-                    vec3(-0.5, 0.5, -0.5)   // position 7
-                );
-
                 vec3 position;
+                vec3 color;
 
                 // vertex indicies (left is positions above, and right is vertex numbers (gl_VertexID))
                 // 0:  (0, 3, 14, 17, 24, 27)
@@ -57,37 +49,53 @@ class ExperimentalCube {
                 // 6:  (5, 18, 21, 25)
                 // 7:  (7, 12, 16, 33)
 
+                //const color1 = [255/255, 173/255, 173/255]; // red
+                //const color2 = [255/255, 214/255, 165/255]; // orange
+                //const color3 = [253/255, 255/255, 182/255]; // yellow
+                //const color4 = [202/255, 255/255, 191/255]; // green
+                //const color5 = [155/255, 246/255, 255/255]; // light blue
+                //const color6 = [160/255, 196/255, 255/255]; // dark blue
+                //const color7 = [189/255, 178/255, 255/255]; // purple
+                //const color8 = [255/255, 198/255, 255/255]; // pink
+
                 // assign vertex position based on gl_VertexID (same concept as indexed cube)
+                // also assign color based on gl_VertexID
                 switch (gl_VertexID) {
                     case 0: case 3: case 14: case 17: case 24: case 27:
-                        position = vertices[0];
+                        position = vec3(0.5, 0.5, 0.5);
+                        color = vec3(255/255, 173/255, 173/255);
                         break;
                     case 1: case 13: case 32: case 35:
-                        position = vertices[1];
+                        position = vec3(-0.5, 0.5, 0.5);
+                        color = vec3(255/255, 214/255, 165/255);
                         break;
                     case 2: case 4: case 19: case 31:
-                        position = vertices[2];
+                        position = vec3(-0.5, -0.5, 0.5);    // position 2
+                        color = vec3(253/255, 255/255, 182/255);
                         break;
                     case 6: case 10: case 20: case 22: case 30: case 34:
-                        position = vertices[3];
+                        position = vec3(-0.5, -0.5, -0.5);   // position 3
+                        color = vec3(202/255, 255/255, 191/255);
                         break;
                     case 9: case 23: case 26: case 28:
-                        position = vertices[4];
+                        position = vec3(0.5, -0.5, -0.5);   // position 4
+                        color = vec3(155/255, 246/255, 255/255);
                         break;
                     case 8: case 11: case 15: case 29:
-                        position = vertices[5];
+                        position = vec3(0.5, 0.5, -0.5);   // position 5
+                        color = vec3(160/255, 196/255, 255/255);
                         break;
                     case 5: case 18: case 21: case 25:
-                        position = vertices[6];
+                        position = vec3(0.5, -0.5, 0.5);   // position 6
+                        color = vec3(189/255, 178/255, 255/255);
                         break;
                     case 7: case 12: case 16: case 33:
-                        position = vertices[7];
+                        position = vec3(-0.5, 0.5, -0.5);   // position 7
+                        color = vec3(255/255, 198/255, 255/255);
                         break;
                 }
-
-                // negative x face is incorrect
                 
-
+                vColor = vec4(color, 1.0);
 
                 // Add switch case here to choose which position for each vertexID
                 // do the same for color, and assign it to vColor to use in the 
@@ -102,17 +110,20 @@ class ExperimentalCube {
             //uniform vec4 color;
             out vec4 fColor;
 
-            //void main() {
-            //    fColor = vec4(1.0, 1.0, 0.0, 1.0);
-            //}
+            in vec4 vColor;
+
+
+            void main() {
+                fColor = vColor;
+            }
 
             // Front facing (yellow, correct) and back facing (green, incorrect) colors
-            const vec4 frontColor = vec4(1.0, 1.0, 0.0, 1.0);
+            /*const vec4 frontColor = vec4(1.0, 1.0, 0.0, 1.0);
             const vec4 backColor = vec4(0.0, 1.0, 0.0, 1.0);
 
             void main() {
                 fColor = gl_FrontFacing ? frontColor : backColor;
-            }
+            }*/
 
         `;
 
