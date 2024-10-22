e299761e2495e63f8f0e2d8945757f7785c52e8d
A-M-D-R-3-W <holygsauce@gmail.com>
2024-10-21 17:31:27 -0700

    Experimental cube color fixed - had to remove division in vertex shader

--- Assignment-3/ExperimentalCube.js
+++ Assignment-3/ExperimentalCube.js
@@ -9,19 +9,14 @@
 // choose the color of each vertex
 
 // GLSL template literal for syntax highlighting in vertex/fragment shaders
-function glsl(strings, ...values) {
+function glsl(strings) {
     return strings.raw[0];
 }
 
 class ExperimentalCube {
     constructor(gl, vertexShader, fragmentShader) {
 
-
-    
-        
         vertexShader ||= glsl`
-            //uniform mat4 P;  // Projection transformation
-            //uniform mat4 MV; // Model-view transformation
 
             uniform mat4 P;  // Projection transformation
             uniform mat4 MV; // Model-view transformation
@@ -30,78 +25,48 @@ class ExperimentalCube {
         
             void main() {
 
-                // Vertex positions
-                // 12 triangles (two triangles per face)
-                // 3 vertices per triangle
-                // 3 components per vertex (x, y, z)
-                // Counter-clockwise winding order
-
                 vec3 position;
                 vec3 color;
 
-                // vertex indicies (left is positions above, and right is vertex numbers (gl_VertexID))
-                // 0:  (0, 3, 14, 17, 24, 27)
-                // 1:  (1, 13, 32, 35)
-                // 2:  (2, 4, 19, 31)
-                // 3:  (6, 10, 20, 22, 30, 34)
-                // 4:  (9, 23, 26, 28)
-                // 5:  (8, 11, 15, 29)
-                // 6:  (5, 18, 21, 25)
-                // 7:  (7, 12, 16, 33)
-
-                //const color1 = [255/255, 173/255, 173/255]; // red
-                //const color2 = [255/255, 214/255, 165/255]; // orange
-                //const color3 = [253/255, 255/255, 182/255]; // yellow
-                //const color4 = [202/255, 255/255, 191/255]; // green
-                //const color5 = [155/255, 246/255, 255/255]; // light blue
-                //const color6 = [160/255, 196/255, 255/255]; // dark blue
-                //const color7 = [189/255, 178/255, 255/255]; // purple
-                //const color8 = [255/255, 198/255, 255/255]; // pink
-
                 // assign vertex position based on gl_VertexID (same concept as indexed cube)
                 // also assign color based on gl_VertexID
+                // note: division by 255 DOES NOT work in GLSL, so normalized manually
                 switch (gl_VertexID) {
                     case 0: case 3: case 14: case 17: case 24: case 27:
-                        position = vec3(0.5, 0.5, 0.5);
-                        color = vec3(255/255, 173/255, 173/255);
+                        position = vec3(0.5, 0.5, 0.5);      // position 0
+                        color = vec3(1.0, 0.6784, 0.6784);   // red
                         break;
                     case 1: case 13: case 32: case 35:
-                        position = vec3(-0.5, 0.5, 0.5);
-                        color = vec3(255/255, 214/255, 165/255);
+                        position = vec3(-0.5, 0.5, 0.5);     // position 1
+                        color = vec3(1.0, 0.8392, 0.6471);   // orange
                         break;
                     case 2: case 4: case 19: case 31:
                         position = vec3(-0.5, -0.5, 0.5);    // position 2
-                        color = vec3(253/255, 255/255, 182/255);
+                        color = vec3(0.9922, 1.0, 0.7137);   // yellow
                         break;
                     case 6: case 10: case 20: case 22: case 30: case 34:
                         position = vec3(-0.5, -0.5, -0.5);   // position 3
-                        color = vec3(202/255, 255/255, 191/255);
+                        color = vec3(0.7922, 1.0, 0.7490);   // green
                         break;
                     case 9: case 23: case 26: case 28:
                         position = vec3(0.5, -0.5, -0.5);   // position 4
-                        color = vec3(155/255, 246/255, 255/255);
+                        color = vec3(0.6078, 0.9647, 1.0);  // light blue
                         break;
                     case 8: case 11: case 15: case 29:
-                        position = vec3(0.5, 0.5, -0.5);   // position 5
-                        color = vec3(160/255, 196/255, 255/255);
+                        position = vec3(0.5, 0.5, -0.5);    // position 5
+                        color = vec3(0.6275, 0.7686, 1.0);  // dark blue
                         break;
                     case 5: case 18: case 21: case 25:
-                        position = vec3(0.5, -0.5, 0.5);   // position 6
-                        color = vec3(189/255, 178/255, 255/255);
+                        position = vec3(0.5, -0.5, 0.5);    // position 6
+                        color = vec3(0.7412, 0.6980, 1.0);  // purple
                         break;
                     case 7: case 12: case 16: case 33:
                         position = vec3(-0.5, 0.5, -0.5);   // position 7
-                        color = vec3(255/255, 198/255, 255/255);
+                        color = vec3(1.0, 0.7765, 1.0);     // pink
                         break;
                 }
                 
                 vColor = vec4(color, 1.0);
-
-                // Add switch case here to choose which position for each vertexID
-                // do the same for color, and assign it to vColor to use in the 
-                // fragment shader
-
-                // Choose the appropriate vertex based on gl_VertexID
                 gl_Position = P * MV * vec4(position, 1.0);
             }
         `;
